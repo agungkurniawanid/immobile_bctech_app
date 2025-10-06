33163341a948d7de6d7bdd75ea5bc029b30281a0
Agung Kurniawan <agungklewang26@gmail.com>
2025-10-06 14:27:18 +0700

    update

---
+++ lib/screens/history/history_inpage_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HistoryInpageScreen extends ConsumerStatefulWidget {
+  const HistoryInpageScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _HistoryInpageScreenState();
+}
+
+class _HistoryInpageScreenState extends ConsumerState<HistoryInpageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
